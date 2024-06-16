e13e4409385d925517c34f6d352ef0983f973e5e
Caio Leite <caio.cobucci@gmail.com>
2024-06-16 19:41:08 +0200

    Initial commit

---
+++ src/main/java/com/iaco/forumhub/ForumhubApplication.java
@@ -0,0 +1,13 @@
+package com.iaco.forumhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumhubApplication.class, args);
+	}
+
+}
